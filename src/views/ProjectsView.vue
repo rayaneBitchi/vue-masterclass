1352a1124189e07d88873e81e2c5f695b9cc1817
rayaneBitchi <rayanebitchi@icloud.com>
2024-11-13 01:28:20 -0500

    working routes

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+  <div>
+    <h1>Projects Page</h1>
+    <RouterLink to="/" >Home Page</RouterLink>
+  </div>
+</template>
+<style scoped></style>
---
+++ src/views/SingleProjectView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const projectId = route.params?.id
+
+console.log(projectId)
+</script>
+<template>
+  <div>
+    <h1>Project {{ projectId }} Page</h1>
+    <RouterLink to="/">Home Page</RouterLink>
+  </div>
+</template>
+<style scoped></style>
